e882c6553e03dd4f1905c9190d819ab072745279
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-04-15 04:47:20 +0000

    first_app

---
+++ app/controllers/public/registrations_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class Public::RegistrationsController < Devise::RegistrationsController
+ before_action :configure_permitted_parameters, if: :devise_controller?
+
+ def after_sign_in_path_for(resource)
+   about_path
+ end
+
+
+
+ protected
+
+ def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :email, :post_code, :address, :phone_number])
+ end
+end
