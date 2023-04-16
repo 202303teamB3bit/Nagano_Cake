5af6d27a3d6a427ef2fc3d4b16e8a15f8ae1664f
narumifumiya <124240213+narumifumiya@users.noreply.github.com>
2023-04-16 06:58:22 +0000

    退会機能追加:

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -19,9 +19,15 @@ class Public::CustomersController < ApplicationController
   end
 
   def unsubscribe
+    @customer = current_customer
   end
 
   def withdraw
+    @customer = current_customer
+    @customer.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行しました。"
+    redirect_to root_path
   end
 
 
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,8 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+ has_many :address, dependent: :destroy
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :first_name_kana, presence: true
