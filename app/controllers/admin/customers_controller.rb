a87591fecda8a2d4377a71bd1bc561251e8e4920
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-04-17 08:13:34 +0000

    finished_admin/customer/edit_page

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -10,4 +10,22 @@ class Admin::CustomersController < ApplicationController
   def edit
     @customer = Customer.find(params[:id])
   end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:notice] = "会員情報が更新されました"
+      redirect_to admin_customer_path(@customer)
+    else
+      flash[:alert] = "会員情報の更新に失敗しました"
+      render :edit
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana,
+                   :post_code, :address, :phone_number, :email, :is_deleted)
+  end
 end
