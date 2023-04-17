e7d24981726c1b957297ab4deeab20281e9c6be4
Bufo <124179824+bufo1129@users.noreply.github.com>
2023-04-17 02:22:25 +0000

    emun導入記述追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -32,7 +32,7 @@ class Admin::ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active, :sales_status, :image)
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active, :image)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
 
   has_one_attached :image
-  enum sales_status: { sale: 0, discontinued: 1 }
+  enum is_active: { sale: 0, discontinued: 1 }
 
   def get_image
     unless image.attached?
