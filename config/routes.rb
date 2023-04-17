d28d1e2d0a6fcb1aa559a199b099bd4f0259e2db
narumifumiya <124240213+narumifumiya@users.noreply.github.com>
2023-04-17 07:36:43 +0000

    会員側商品ページ作成中

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     get 'customers/unsubscribe' => 'customers#unsubscribe'
     patch '/customers/withdraw' => 'customers#withdraw'
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+    resources :items, only: [:index, :show]
   end
 
   # 管理者用
