6396b8b8d23d75cbcac3be03bc2d6c057111bf19
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-04-19 09:10:29 +0000

    fished_orders/new

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,6 +3,9 @@ class Public::OrdersController < ApplicationController
 
   def new
     @order = Order.new
+    @customer = Customer.find(current_customer.id)
+    @addresses = current_customer.addresses
+
   end
 
   def create
@@ -26,6 +29,27 @@ class Public::OrdersController < ApplicationController
     @order = Order.new(order_params)
     @cart_items = current_customer.cart_items
     @total = 0
+
+    if params[:order][:address_option] == "0"
+      @order.ship_postcode = current_customer.postcode
+      @order.ship_address = current_customer.address
+      @order.ship_name = current_customer.first_name + current_customer.last_name
+
+      elsif params[:order][:address_option] == "1"
+        ship = Destination.find(params[:order][:customer_id])
+        @order.ship_postcode = ship.postcode
+        @order.ship_address = ship.address
+        @order.ship_name = ship.name
+
+        elsif params[:order][:address_option] == "2"
+          @order.ship_postcode = params[:order][:ship_postcode]
+          @order.ship_address = params[:order][:ship_address]
+          @order.ship_name = params[:order][:ship_name]
+
+        else
+          @order = Order.new(order_params)
+          render :new
+        end
   end
 
   def complete
@@ -36,7 +60,6 @@ class Public::OrdersController < ApplicationController
   def order_params
     params.require(:order).permit(:name, :post_code,
                                   :address, :payment_method,
-                                  :shipping_fee, :billing_amont,)
-  end
-
-end
+                                  :shipping_fee, :billing_amont)
+ end
+ end
