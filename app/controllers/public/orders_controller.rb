6f6e12cd4b4bb473c61960eef9a11de1ed9cb0ce
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-04-19 03:48:57 +0000

    edit_orders/check

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,10 +2,16 @@ class Public::OrdersController < ApplicationController
   before_action :authenticate_customer!
 
   def new
-    @customer = current_customer
+    @order = Order.new
   end
 
   def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to orders_path, notice: 'Order was successfully created.'
+    else
+      render :new
+    end
   end
 
   def index
@@ -15,8 +21,18 @@ class Public::OrdersController < ApplicationController
   end
 
   def check
+    @order = Order.new(order_params)
+    @cart_items = current_customer.cart_items
   end
 
   def complete
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:name, :post_code,
+                                  :address, :payment_method)
+  end
+
 end
