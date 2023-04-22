903b3f33c0211a05fae86e4e6ca7e9ba59212eb2
narumifumiya <124240213+narumifumiya@users.noreply.github.com>
2023-04-22 04:33:03 +0000

    会員商品検索機能追加

---
+++ app/controllers/public/searches_controller.rb
@@ -0,0 +1,16 @@
+class Public::SearchesController < ApplicationController
+
+  def search
+    @word = params[:word]
+    @search = params[:search]
+    @range = params[:range]
+
+    if @range == "商品"
+      @items = Item.looks(@search, @word)
+      @genres = Genre.all
+    end
+  end
+
+
+
+end
